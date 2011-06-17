be722c2be31bca37e05611335b330bceb0ee3604
Mike Ferguson <mfergs7@gmail.com>
2011-06-17 18:36:15 +0000

    (mini_max_tutorials) add package

---
+++ mini_max_tutorials/nodes/button_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+""" Example code of how to access button state. """
+
+# We always import roslib, and load the manifest to handle dependencies
+import roslib; roslib.load_manifest('button_test')
+import rospy
+
+# arbotix_msgs defines the Digital message, which tells us the state
+#  of the button (a digital pin)
+from arbotix_msgs.msg import *
+
+class ButtonTest:
+    
+    def buttonCb(self, msg):
+        # the message is a Digital message, value tells us the state of the button
+        if msg.value == 0:
+            print "Button pressed!"
+
+    def __init__(self):
+        # we have to initialize the node, with a name in the ROS graph
+        rospy.init_node('button_test')
+
+        # subscribe to the green_button value. 
+        rospy.Subscriber('/arbotix/green_button',Digital,self.buttonCb)
+
+        # everything will be handled in the callback, just spin
+        rospy.spin()
+
+# this quick check means that the following code runs ONLY if this is in the main 
+#  file -- if we "import button_test" in another file, this code will not execute
+if __name__ == "__main__":
+    bt = ButtonTest()
